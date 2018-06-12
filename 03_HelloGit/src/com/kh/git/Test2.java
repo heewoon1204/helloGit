6defb7f4a75e674b4050cf089df8196d8ccf1001
heewoon1204 <heewoon1204@naver.com>
2018-06-12 13:25:50 +0900

    test1 브랜치 시작!!

--- 03_HelloGit/src/com/kh/git/Test2.java
+++ 03_HelloGit/src/com/kh/git/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("깃깃기신밍러ㅏㅣㅁ너리ㅏㅁㄴ어");
 		System.out.println("깃깃기신밍러ㅏㅣㅁ너리ㅏㅁㄴ어");
+		
+		System.out.println("test1 브랜치 시작!!");
 	}
 
 }
